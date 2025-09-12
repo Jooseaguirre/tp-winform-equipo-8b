0766b33c30d94d622677b7507139f987570b812b
erikaespeche <especherika96@gmail.com>
2025-09-12 20:34:29 -0300

    Se agrego la clase 'AccesoDatos'

--- negocio/ArticuloNegocio.cs
+++ negocio/ArticuloNegocio.cs
@@ -14,67 +14,53 @@ namespace negocio
         public List<Articulo> Listar()
         {
             List<Articulo> listaArticulos = new List<Articulo>();
-            SqlConnection conexion = new SqlConnection();
-            SqlCommand comandoArticulo = new SqlCommand();
-            SqlDataReader lectorArticulo;
+            AccesoDatos datos = new AccesoDatos();
            
             
 
             try
             {
-                //CONEXION A BASE DE DATOS
-                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true";
-                conexion.Open();
-
-
-                //ARTICULOS
-                comandoArticulo.CommandType = System.Data.CommandType.Text;
-                //comandoArticulo.CommandText = "SELECT a.Id, a.Nombre, a.Codigo, a.Descripcion, a.IdMarca, a.IdCategoria, a.Precio, i.ImagenUrl FROM ARTICULOS a LEFT JOIN IMAGENES i ON i.IdArticulo = a.Id;";
-                comandoArticulo.CommandText = "SELECT A.Id, A.Nombre, A.Codigo, A.Descripcion, M.Descripcion AS Marca, C.Descripcion AS Categoria, A.Precio, I.ImagenUrl FROM ARTICULOS A LEFT JOIN MARCAS M ON A.IdMarca = M.Id LEFT JOIN CATEGORIAS C ON A.IdCategoria = C.Id LEFT JOIN IMAGENES I ON A.Id = I.IdArticulo;";
-                comandoArticulo.Connection = conexion;
-
+                //TRAER LISTA DE ARTICULOS
+                datos.setearConsulta("SELECT A.Id, A.Nombre, A.Codigo, A.Descripcion, M.Descripcion AS Marca, C.Descripcion AS Categoria, A.Precio, I.ImagenUrl FROM ARTICULOS A LEFT JOIN MARCAS M ON A.IdMarca = M.Id LEFT JOIN CATEGORIAS C ON A.IdCategoria = C.Id LEFT JOIN IMAGENES I ON A.Id = I.IdArticulo;");
+                datos.ejecutarLectura();
 
            
                 //leer ARTICULOS
-                lectorArticulo = comandoArticulo.ExecuteReader();
-                while (lectorArticulo.Read())
+                while (datos.Lector.Read())
                 {
                     Articulo aux = new Articulo();
-                    aux.Id = (int)lectorArticulo["Id"];
-                    aux.Codigo = (string)lectorArticulo["Codigo"];
-                    aux.Nombre = (string)lectorArticulo["Nombre"];
-                    aux.Descripcion = (string)lectorArticulo["Descripcion"];
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Codigo = (string)datos.Lector["Codigo"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
                     aux.Marca = new Marca();
-                    aux.Marca.Descripcion = (string)lectorArticulo["Marca"];
+                    aux.Marca.Descripcion = (string)datos.Lector["Marca"];
                     aux.Categoria = new Categoria();
-                    aux.Categoria.Descripcion = lectorArticulo["Categoria"] != DBNull.Value ? (string)lectorArticulo["Marca"] : "SIN DATOS";
-                    aux.precio = (decimal)lectorArticulo["Precio"];
+                    aux.Categoria.Descripcion = datos.Lector["Categoria"] != DBNull.Value ? (string)datos.Lector["Marca"] : "SIN DATOS";
+                    aux.precio = (decimal)datos.Lector["Precio"];
 
                     aux.Imagenes = new List<Imagen>();
                     Imagen imagen = new Imagen();
-                    imagen.ImagenUrl = (string)lectorArticulo["ImagenUrl"];
+                    imagen.ImagenUrl = (string)datos.Lector["ImagenUrl"];
                     imagen.IdArticulo = aux.Id;
                     aux.Imagenes.Add(imagen);
 
                     listaArticulos.Add(aux);
                 }
-                //lectorArticulo.Close();
-
-                
-
+                return listaArticulos;
 
 
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Error al leer los datos: {ex.Message}");
+                throw ex;
             }
             finally
             {
-                conexion.Close();
+                datos.cerrarConexion();
             }
 
-            return listaArticulos;
+            //return listaArticulos;
 
         }
     }
